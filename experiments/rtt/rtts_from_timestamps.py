224ca3c989f0f7c4a50b41bf1e324c959268edce
Zeno Koller <zeno.koller@gmail.com>
2018-02-26 10:22:28 +0100

    Refactor rtt from timestamps

--- experiments/rtt/rtts_from_timestamps.py
+++ experiments/rtt/rtts_from_timestamps.py
@@ -13,18 +13,28 @@ def rtts_from_flow(flow: Tuple[str, pd.DataFrame]) -> pd.DataFrame:
     associations: Dict[int, pd.Timestamp] = {}  # When was the associated ts_val of echo first seen?
     seen_echos: Set[int] = {0}  # Only consider first echo to prevent overestimation
 
-    for packet in packets_df.itertuples():
-        ts_val, ts_ecr = packet.ts_val, packet.ts_ecr
-        if ts_val not in observations:
-            observations[ts_val] = packet.timestamp
-        if ts_ecr in observations and ts_ecr not in seen_echos:
-            associations[ts_val] = observations[ts_ecr]
-            seen_echos.add(ts_ecr)
-        associated = associations.get(ts_ecr, None)
+    def record_observation(val: int, observer_timestamp: pd.Timestamp):
+        if val not in observations:
+            observations[val] = observer_timestamp
+
+    def make_association(value: int, echo: int):
+        if echo in observations and echo not in seen_echos:
+            associations[value] = observations[echo]
+            seen_echos.add(echo)
+
+    def record_rtt(echo: int, observer_timestamp: pd.Timestamp):
+        associated = associations.get(echo, None)
         if associated is not None:
-            rtts.append(RTT(timestamp=packet.timestamp,
+            rtts.append(RTT(timestamp=observer_timestamp,
                             flow_hash=flow_hash,
-                            rtt=packet.timestamp - associated))
-            del associations[ts_ecr]
+                            rtt=observer_timestamp - associated))
+            del associations[echo]
+
+    for packet in packets_df.itertuples():
+        ts_val, ts_ecr, timestamp = packet.ts_val, packet.ts_ecr, packet.timestamp
+
+        record_observation(ts_val, timestamp)
+        make_association(ts_val, ts_ecr)
+        record_rtt(ts_ecr, timestamp)
 
     return pd.DataFrame(rtts, columns=RTT._fields)
