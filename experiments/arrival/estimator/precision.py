a4606de56a55fc1715aa7147e77d25798642a063
Zeno Koller <zeno.koller@gmail.com>
2018-03-28 16:03:55 +0200

    Only consider loss precision

--- experiments/arrival/estimator/precision.py
+++ experiments/arrival/estimator/precision.py
@@ -1,22 +1,15 @@
 from typing import Optional
 
 from estimator.events import Events
-from estimator.find_events import find_events
+from estimator.find_events import find_events, find_losses
 from simulator.ground_truth.ground_truth import GroundTruth
 from synchronizer.alignment import Alignment
 
 
-def precision(alignment: Alignment, ground_truth: GroundTruth) -> float:
-    """`Precision` measure for an alignment. Yields -1 if there were no events at all."""
-    events = find_events(alignment)
-    true_positives = count_true_positives(events, ground_truth)
-    total_events = events.number_of_events
+def loss_precision(alignment: Alignment, ground_truth: GroundTruth) -> float:
+    """`Precision` measure for an alignment, which only considers losses. Yields -1 if there were no
+    losses at all."""
+    losses = find_losses(alignment)
+    true_positives = len(set(losses).intersection(set(ground_truth.losses)))
+    total_events = len(losses)
     return true_positives / total_events if total_events > 0 else -1
-
-
-def count_true_positives(events: Events, ground_truth: GroundTruth) -> int:
-    losses, delays, dupes = events
-    gt_losses, gt_delays, gt_dupes = ground_truth
-    return len(set(losses).intersection(set(gt_losses))) + \
-           len(set(dupes).intersection(set(gt_dupes))) + \
-           len([key for key, value in delays.items() if value == gt_delays.get(key, None)])
