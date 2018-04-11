98cf858418ba656951603a21f659c76c666dd9c3
Zeno Koller <zeno.koller@gmail.com>
2018-04-11 11:04:46 +0200

    Remove offset from sequence generator

--- experiments/arrival/generator/sequence.py
+++ experiments/arrival/generator/sequence.py
@@ -2,20 +2,14 @@ from random import Random
 from typing import Iterable, Callable
 
 
-def generate_sequence(get_next_bits: Callable[[int], int],
-                      symbol_bits: int,
-                      offset: int = None) -> Iterable[int]:
-    if offset is not None:
-        get_next_bits(offset)
+def generate_sequence(get_next_bits: Callable[[int], int], symbol_bits: int) -> Iterable[int]:
     while True:
         yield get_next_bits(symbol_bits)
 
 
-def generate_random_sequence(symbol_bits: int,
-                             offset: int = None,
-                             seed: int = None) -> Iterable[int]:
+def generate_random_sequence(symbol_bits: int, seed: int = None) -> Iterable[int]:
     r = Random(seed)
-    return generate_sequence(lambda k: r.getrandbits(k), symbol_bits, offset=offset)
+    return generate_sequence(lambda k: r.getrandbits(k), symbol_bits)
 
 # Generate a sequence:
 #   gen = generate_random_sequence(8, seed=42)
