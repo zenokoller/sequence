6b9602f31869c41a80902eec14b92a8642b03cd7
Zeno Koller <zeno.koller@gmail.com>
2018-05-30 10:48:35 +0200

    Make http reporter more flexible

---
+++ src/reporter/exceptions.py
@@ -0,0 +1,2 @@
+class ReporterError(Exception):
+    pass
--- src/reporter/http_reporter.py
+++ src/reporter/http_reporter.py
@@ -1,24 +1,45 @@
 import logging
+from typing import Coroutine, Callable, Any
 
 from aiohttp import web
 
-from detector.events import Loss, Event, Reordering
+from detector.events import Event, Reordering, Loss
+from reporter.exceptions import ReporterError
 from reporter.reporter import Reporter
+from utils.coroutine import coroutine
 
-JSON_REPORTER_PORT = 9090
+HTTP_REPORTER_PORT = 9090
+
+
+@coroutine
+def count_accumulator(period) -> Coroutine[dict, Event, None]:
+    """Accumulates packet, loss and reordering counts _without_ relying on explicit `Receive`
+    events. Instead uses the difference in the offset of subsequent packets."""
+    period = int(period)
+    packets, losses, reorderings, _last_offset = 0, 0, 0, 0
+    while True:
+        event = yield {'packets': packets, 'losses': losses, 'reorderings': reorderings}
+        if isinstance(event, Loss):
+            losses += 1
+        elif isinstance(event, Reordering):
+            reorderings += 1
+        else:
+            raise ReporterError(f'Unexpected event: {event}')
+        packets += (event.offset - _last_offset) % period
+        _last_offset = event.offset
 
 
 class HttpReporter(Reporter):
-    """Simple reporter that collects packet and loss counts and exposes them as JSON on an HTTP
-    endpoint."""
+    """Simple reporter that handles event with an `accumulator` coroutine, which returns a `dict`.
+    Said `dict` is exposed in JSON on an HTTP endpoint: `http://localhost:HTTP_REPORTER_PORT"""
 
-    def __init__(self, period):
+    def __init__(self,
+                 *accumulator_args,
+                 accumulator: Callable[[Any], Coroutine[dict, Event, None]] = count_accumulator):
         logging.getLogger('aiohttp.access').setLevel(logging.WARNING)
-        self.period = int(period)
-        self.packets = 0
-        self.losses = 0
-        self.reorderings = 0
-        self._last_offset = 0
+
+        self.values = {}
+        self.accumulator = accumulator(*accumulator_args)
 
         app = web.Application()
         app.add_routes([web.get('/', self.handle_request)])
@@ -26,25 +47,14 @@ class HttpReporter(Reporter):
 
     async def setup(self):
         await self.runner.setup()
-        site = web.TCPSite(self.runner, port=JSON_REPORTER_PORT)
+        site = web.TCPSite(self.runner, port=HTTP_REPORTER_PORT)
         await site.start()
 
     async def cleanup(self):
         await self.runner.cleanup()
 
     async def handle_event(self, event: Event):
-        if isinstance(event, Loss):
-            self.losses += 1
-        elif isinstance(event, Reordering):
-            self.reorderings += 1
-        else:
-            return
-        self.packets += (event.offset - self._last_offset) % self.period
-        self._last_offset = event.offset
+        self.values = self.accumulator.send(event)
 
     async def handle_request(self, request):
-        return web.json_response({
-            'packets': self.packets,
-            'losses': self.losses,
-            'reorderings': self.reorderings
-        })
+        return web.json_response(self.values)
