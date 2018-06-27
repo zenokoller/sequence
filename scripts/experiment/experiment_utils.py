7bf529ad6b6391e7e47b99647effcb85666aa4f5
Zeno Koller <zeno.koller@gmail.com>
2018-06-27 13:10:59 +0200

    Finalize loss rate experiment again

--- scripts/experiment/experiment_utils.py
+++ scripts/experiment/experiment_utils.py
@@ -11,12 +11,15 @@ def reset_netem(testbed_path: str):
     _ = subprocess.check_call(cmd)
 
 
-def configure_netem(testbed_path: str, config_file: str) -> str:
+def configure_netem(testbed_path: str, config_file: str, blocking: bool = False) -> str:
     print(f'>>> Configuring netem with {config_file}...')
     os.chdir(testbed_path)
     cmd = ['bin/link-config.bash', '-t', '-n', '-c', config_file]
     timestamp = datetime.datetime.now().isoformat()
-    _ = subprocess.run(cmd, stdout=subprocess.PIPE)
+    if blocking:
+        _ = subprocess.check_call(cmd, stdout=subprocess.PIPE)
+    else:
+        _ = subprocess.run(cmd, stdout=subprocess.PIPE)
     return timestamp
 
 
@@ -24,7 +27,7 @@ def repeatedly_configure_netem(testbed_path: str,
                                config_files: List[str],
                                interval: float,
                                log_queue):
-    next_times = [time.time() + (i + 1) * interval for i, _ in enumerate(config_files)]
+    next_times = [time.time() + i * interval for i, _ in enumerate(config_files)]
     for next_time, conf in zip(next_times, config_files):
         time.sleep(max(0, next_time - time.time()))
         timestamp = configure_netem(testbed_path, conf)
--- scripts/experiment/loss_rate_experiment.py
+++ scripts/experiment/loss_rate_experiment.py
@@ -8,14 +8,21 @@ from influxdb import DataFrameClient
 
 from scripts.experiment.base_experiment import BaseExperiment, start_experiment
 from scripts.experiment.experiment_utils import repeatedly_configure_netem, reset_netem
+from scripts.plot.loss_rate import plot
 
 """Run an experiment while cycling configurations."""
+
+TITLE = 'Random Configurations'
+# TITLE = 'Gilbert-Elliott Configurations'
 NETEM_CONFS = [
     'conf/loss_rate/random_0125.conf',
     'conf/loss_rate/random_025.conf',
     'conf/loss_rate/random_05.conf',
     'conf/loss_rate/random_1.conf',
     'conf/loss_rate/random_2.conf',
+    # 'conf/loss_rate/random_4.conf',
+    # 'conf/loss_rate/random_8.conf',
+    # 'conf/loss_rate/random_16.conf',
     # 'conf/loss_rate/ge_0125.conf',
     # 'conf/loss_rate/ge_025.conf',
     # 'conf/loss_rate/ge_05.conf',
@@ -54,10 +61,9 @@ def loss_rate_to_csv(start_time: int, end_time: int, csv_path: str, _: dict):
 
     joined_df = sequence_df.join(netem_df, lsuffix="_sequence", rsuffix="_netem")
 
-    # Use netem packet counts to calculate sequence loss rate as shortcut to getting packet counts
-    # from offsets in each interval
-    joined_df['rate_sequence'] = compute_loss_rate(joined_df, 'losses_sequence', 'packets_netem')
-    joined_df['rate_netem'] = compute_loss_rate(joined_df, 'losses_netem', 'packets_netem')
+    # Use sequence packet counts to netem sequence to prevent netem oddities
+    joined_df['rate_sequence'] = compute_loss_rate(joined_df, 'losses_sequence', 'packets_sequence')
+    joined_df['rate_netem'] = compute_loss_rate(joined_df, 'losses_netem', 'packets_sequence')
 
     # Where netem failed to count packets, set the loss rate to zero
     joined_df['rate_sequence'][joined_df['packets_netem'] == 0] = 0
@@ -80,11 +86,15 @@ if __name__ == '__main__':
     thread = Thread(target=repeatedly_configure_netem,
                     args=(args.testbed_path, NETEM_CONFS, INTERVAL, log_queue))
     thread.start()
-    out_dir = start_experiment(RateExperiment, config='config/rate_2_bit.yml',
-                               out_dir=args.out_dir, testbed_path=args.testbed_path)
+    out_dir = start_experiment(RateExperiment,
+                               config='config/rate_2_bit.yml',
+                               out_dir=args.out_dir,
+                               testbed_path=args.testbed_path)
     thread.join()
 
     with open(os.path.join(out_dir, 'repeated_netem_confs.log'), 'w+') as confs_log:
         while not log_queue.empty():
             timestamp, conf = log_queue.get()
             confs_log.write(f'{timestamp},{conf}\n')
+
+    plot(out_dir, TITLE)
